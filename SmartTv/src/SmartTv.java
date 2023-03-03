8cab1f713f4d42a7a2ddec2a89e09fbc9ac8b0b7
Anderson Alves <anderson.dmc07@gmail.com>
2023-03-02 23:06:26 -0300

    Projeto SmartTV

--- SmartTv/src/SmartTv.java
+++ SmartTv/src/SmartTv.java
@@ -1,5 +1,37 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class SmartTv {
+
+    boolean ligada=false;
+    int canal=1;
+    int volume=25;
+
+    public void ligar() {
+        ligada=true;
     }
+
+    public void desligar() {
+        ligada=false;
+    }
+
+    public void mudarCanal (int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume () {
+        volume++;
+        System.out.println("Aumentando o Volume " +volume);
+    }
+
+    public void diminuirVolume () {
+        volume--;
+        System.out.println("Diminuindo o Volume " +volume);
+    }
+
 }
